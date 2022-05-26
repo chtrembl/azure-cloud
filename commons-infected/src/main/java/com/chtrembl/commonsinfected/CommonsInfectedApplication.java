04dce0506c39fa8af36c9b7bd999d361e2937fea
chtrembl <62187826+chtrembl@users.noreply.github.com>
2022-05-26 15:01:37 -0400

    initial

---
+++ commons-infected/src/main/java/com/chtrembl/commonsinfected/CommonsInfectedApplication.java
@@ -0,0 +1,13 @@
+package com.chtrembl.commonsinfected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsInfectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsInfectedApplication.class, args);
+	}
+
+}
