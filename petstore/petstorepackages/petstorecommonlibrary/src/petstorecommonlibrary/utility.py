8337c6ca4a74bb39db88cc6e52d382259fe0bbaa
Chris Tremblay <chris.tremblay@microsoft.com>
2022-09-19 10:49:53 -0400

    initial commit

---
+++ petstore/petstorepackages/petstorecommonlibrary/src/petstorecommonlibrary/utility.py
@@ -0,0 +1,7 @@
+def getBreeds(animal):
+    if animal == "Dog":
+        return "Afador, American Bulldog, Australian Retriever"
+    elif animal == "Cat":
+        return "Abyssinian, American Bobtail"
+    elif animal == "Fish":
+        return "Goldfish"
