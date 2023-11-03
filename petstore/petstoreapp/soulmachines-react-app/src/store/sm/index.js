f2691658949d7d45b4fd4d42c790c92fa5321130
Chris Tremblay <62187826+chtrembl@users.noreply.github.com>
2023-11-03 16:38:13 -0400

    Update index.js

--- petstore/petstoreapp/soulmachines-react-app/src/store/sm/index.js
+++ petstore/petstoreapp/soulmachines-react-app/src/store/sm/index.js
@@ -622,6 +622,7 @@ export const sendTextMessage = createAsyncThunk('sm/sendTextMessage', async ({ t
     const urlParams = new URLSearchParams(window.location.search);
     const sid = urlParams.get('sid');
     const csrf = urlParams.get('csrf');
+    console.log(`${text} sid:${sid}csrf:${csrf}`);
     persona.conversationSend(`${text} sid:${sid}csrf:${csrf}`);
     return thunk.dispatch(actions.addConversationResult({
       source: 'user',
