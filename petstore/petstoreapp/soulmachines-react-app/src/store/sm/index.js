45af889ab8f0047fc7feb28f5fa81a0e7f796cb3
Chris Tremblay <chris.tremblay@microsoft.com>
2023-11-03 18:49:42 -0400

    updates

--- petstore/petstoreapp/soulmachines-react-app/src/store/sm/index.js
+++ petstore/petstoreapp/soulmachines-react-app/src/store/sm/index.js
@@ -619,9 +619,7 @@ export const sendTextMessage = createAsyncThunk('sm/sendTextMessage', async ({ t
   if (text === '') return thunk.rejectWithValue('submitted empty string!');
   if (scene !== null && persona !== null) {
     persona.conversationSetVariables({ sessionid: 'session id', csrftoken: 'csrf token' });
-    const urlParams = new URLSearchParams(window.location.search);
-    const sid = urlParams.get('sid');
-    const csrf = urlParams.get('csrf');
+    console.log(`${text} sid:${sid}csrf:${csrf}`);
     persona.conversationSend(`${text} sid:${sid}csrf:${csrf}`);
     return thunk.dispatch(actions.addConversationResult({
       source: 'user',
