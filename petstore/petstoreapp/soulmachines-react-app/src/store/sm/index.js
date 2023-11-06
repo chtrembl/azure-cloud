a0e266460998248e778802cd3c24a005403ce3c3
Chris Tremblay <chris.tremblay@microsoft.com>
2023-11-06 14:52:46 -0500

    Update index.js

--- petstore/petstoreapp/soulmachines-react-app/src/store/sm/index.js
+++ petstore/petstoreapp/soulmachines-react-app/src/store/sm/index.js
@@ -623,9 +623,9 @@ export const sendTextMessage = createAsyncThunk('sm/sendTextMessage', async ({ t
     const iframes = document.getElementsByTagName('iframe');
     for (let i = 0; i < iframes.length; i += 1) {
       try {
-          console.log(iframes[i].contentWindow.location.href);
+        console.log(iframes[i].contentWindow.location.href);
       } catch (e) {
-      console.log('Cannot access iframe URL due to same-origin policy:', e);
+        console.log('Cannot access iframe URL due to same-origin policy:', e);
       }
     }
     persona.conversationSend(`${text} sid:${window.top.sid}csrf:${window.parent.csrf}`);
