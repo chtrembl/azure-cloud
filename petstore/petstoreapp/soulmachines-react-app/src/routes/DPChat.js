691c06707ff558c72d8595a0f39becb2420812f3
Chris Tremblay <chris.tremblay@microsoft.com>
2024-01-26 12:01:56 -0500

    Update DPChat.js

--- petstore/petstoreapp/soulmachines-react-app/src/routes/DPChat.js
+++ petstore/petstoreapp/soulmachines-react-app/src/routes/DPChat.js
@@ -66,9 +66,13 @@ function DPChat({
     setTimeout(() => {
       dispatch(
         // get an element and click it to start the conversation
+        console.log('toggleMicrophone click'),
         document.getElementById('toggleMicrophone').click(),
+        console.log('textMessageInput ...'),
         document.getElementById('textMessageInput').value = '...',
+        console.log('sendMessageButton click'),
         document.getElementById('sendMessageButton').click(),
+        console.log('toggleMicrophone click'),
         document.getElementById('toggleMicrophone').click(),
       );
     }, 2000);
