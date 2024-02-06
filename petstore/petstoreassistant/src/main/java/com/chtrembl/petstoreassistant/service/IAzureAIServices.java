9cffa14d34622d625d12ad41c067887804414ebf
Chris Tremblay <chris.tremblay@microsoft.com>
2024-02-05 22:12:51 -0500

    rate limit updates

--- petstore/petstoreassistant/src/main/java/com/chtrembl/petstoreassistant/service/IAzureAIServices.java
+++ petstore/petstoreassistant/src/main/java/com/chtrembl/petstoreassistant/service/IAzureAIServices.java
@@ -4,7 +4,7 @@ import com.chtrembl.petstoreassistant.model.DPResponse;
 import com.chtrembl.petstoreassistant.service.AzureAIServices.Classification;
 
 public interface IAzureAIServices {
-    public DPResponse classification(String text);
-    public DPResponse completion(String text, Classification classification);
+    public DPResponse classification(String text, String sessionID);
+    public DPResponse completion(String text, Classification classification, String sessionID);
     public DPResponse search(String text,Classification classification);
 }
