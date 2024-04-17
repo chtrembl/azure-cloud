b274bfa7fef7eee4ed0d618bc6bf528e4960b6df
Daniyal Azhar <daniyalazhar@Daniyals-MacBook-Pro.local>
2024-04-16 19:16:25 -0700

    updating

--- petcloudstore/petcloudstore/src/test/java/com/dazhar/petcloudstore/controller/WebAppControllerTest.java
+++ petcloudstore/petcloudstore/src/test/java/com/dazhar/petcloudstore/controller/WebAppControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
 import com.dazhar.petcloudstore.controller.WebAppController;
 import com.dazhar.petcloudstore.model.Category;
 import com.dazhar.petcloudstore.model.Pet;
